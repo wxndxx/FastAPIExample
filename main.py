f3caf062dd36e26aee7b899ccbf296767c5b3fd6
r.klimov <r.klimov@timeweb.ru>
2024-03-15 15:18:00 +0300

    netaddr

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+from netaddr import IPAddress
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/",
+         include_in_schema=False)
 async def root():
-    return {"message": "Hello World"}
+    return RedirectResponse("/docs")
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get('/ip')
+async def get_user_ip(request: Request):
+    ip_version = IPAddress(request.client.host).version
+    return {"message": f'Your IPv{ip_version} is {request.client.host}'}
